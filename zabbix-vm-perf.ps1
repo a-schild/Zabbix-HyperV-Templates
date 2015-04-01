5a4f941934471a13a97760ad4e6dc60f01683d9b
Dmitry Sarkisov <ait.meijin@gmail.com>
2015-04-01 17:44:17 +0300

    Fixed Guest VM item discovery.

--- zabbix-vm-perf.ps1
+++ zabbix-vm-perf.ps1
@@ -2,7 +2,7 @@
 Zabbix Agent PowerShell script for Hyper-V monitoring 
 
 
-opyright (c) 2015, Dmitry Sarkisov <ait.meijin@gmail.com>
+Copyright (c) 2015, Dmitry Sarkisov <ait.meijin@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
 to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
@@ -12,7 +12,6 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 
 
 #>
@@ -25,11 +24,14 @@ param(
 	[string]$VMObject
 )
 
+$hostname = Get-WmiObject win32_computersystem | Select-Object -ExpandProperty name
+
+$VMName = $VMName.Replace("_" + $hostname, '')
 
 <# Zabbix Hyper-V Virtual Machine Discovery #>
 if ($QueryName -eq '') {
     
-	$hostname = Get-WmiObject win32_computersystem | Select-Object -ExpandProperty name
+	
     $colItems = Get-VM
 
     write-host "{"
