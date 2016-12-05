6826f910117da03f18a98b59747a58a7ac0a2a37
Dmitry Sarkisov <ait.meijin@gmail.com>
2016-12-05 14:51:25 +0300

    Compatibility branch for Zabbix Server 3.0 	Inital commit.

--- zabbix-vm-perf.ps1
+++ zabbix-vm-perf.ps1
@@ -2,7 +2,7 @@
 Zabbix Agent PowerShell script for Hyper-V monitoring 
 
 
-Copyright (c) 2015, Dmitry Sarkisov <ait.meijin@gmail.com>
+Copyright (c) 2015,2016 Dmitry Sarkisov <ait.meijin@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
 to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
